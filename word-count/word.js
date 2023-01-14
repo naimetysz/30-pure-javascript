1b9399c336526d836be87da746b4f920febd48b8
naimetysz <nnaimetuysuz@gmail.com>
2023-01-14 10:35:42 +0300

    stili duzelt yorum ekle

--- word-count/word.js
+++ word-count/word.js
@@ -2,7 +2,7 @@ var text = document.querySelector('.text')
   var words = document.querySelector('.words')
   var characters = document.querySelector('.characters')
   text.addEventListener('input',()=>{
-      characters.textContent = text.ariaValueMax.length
-      var txt =text.ariaValueMax.trim()
+      characters.textContent = text.value.length
+      var txt =text.value.trim()
       words.textContent =txt.split(/\s+/).filter((item)=> item).length
   })
